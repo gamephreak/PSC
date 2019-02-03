78d849e04382e7c79b6584fed3b0ae8b8959ca0e
Kirk Scheibelhut <kjs@scheibo.com>
2019-02-02 19:06:34 -0800

    Add pseudo weather

--- src/field.ts
+++ src/field.ts
@@ -1,10 +1,13 @@
 export type Terrain = 'Electric'|'Grassy'|'Psychic'|'Misty';
 export type Weather =
     'Sand'|'Sun'|'Rain'|'Hail'|'Harsh Sunshine'|'Heavy Rain'|'Strong Winds';
+export type PseudoWeather =
+  'Gravity'|'Trick Room'|'Magic Room'|'Wonder Room'|
+  'Fairy Lock'|'Ion Deluge'|'Mud Sport'|'Water Sport';
 
 export interface Field {  // class
+  // TODO need to keep counts!
   terrain?: Terrain;
   weather?: Weather;
-  gravity?: boolean;
-  trickroom?: boolean;
+  pseudoWeather?: PseudoWeather[];
 }
